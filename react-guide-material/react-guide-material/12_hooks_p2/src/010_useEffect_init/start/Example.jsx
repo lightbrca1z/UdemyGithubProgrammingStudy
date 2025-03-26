c1457a59d29569825784fc8f263f1babacfc9e1f
lightbrca1z <quwieorpt@gmail.com>
2025-03-26 11:40:35 +0900

    UdemyReactApp 20250326-11:40

---
+++ react-guide-material/react-guide-material/12_hooks_p2/src/010_useEffect_init/start/Example.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const Example = () => {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    console.log('useEffect is called');
+    window.setInterval(() => {
+      setTime(prev => prev + 1);
+    },1000);
+    }, [])  
+  // window.setInterval(() => {
+  //   setTime(prev => prev + 1);
+  // },1000);
+
+  useEffect(() => {
+    console.log('updated');
+  },[time])
+
+  useEffect(() => {
+    document.title = 'counter:' + time;
+    window.localStorage.setItem('time-key', time);
+  },[time])
+
+  return(
+    <h3>
+      <time>{time}</time>
+      <span>秒経過</span>
+    </h3>
+  );
+};
+
+export default Example;
---
+++ react-guide-material/react-guide-material/12_hooks_p2/src/030_useEffect_cleanup/start/Example.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+const Example = () => {
+  const [isDisp, setIsDisp] = useState(true);
+return (
+  <>
+    {isDisp && <Timer/>}
+    <button onClick={() => setIsDisp(prev => !prev)}>Toggle</button>
+  </>
+)
+}
+
+const Timer = () => {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    console.log('init');
+    let intervalId = null;
+    intervalId = window.setInterval(() => {
+      setTime(prev => prev + 1);
+    }, 1000);
+    return () => {
+      // console.log('end');
+      window.clearInterval(intervalId);
+    }
+  }, [])
+  
+  useEffect(() => {
+    // console.log('updated');
+
+    document.title = 'counter:' + time;
+    window.localStorage.setItem('time-key-end', time)
+
+    return () => {
+      // console.log('updated end');
+    }
+  }, [time]);
+
+  return (
+    <h3>
+      <time>{time}</time>
+      <span>秒経過</span>
+    </h3>
+    );
+};
+
+export default Example;
